e4d6eda43d6672620c01cf1b6bebbd3bb86fbb42
谭仁 <672019554@qq.com>
2019-04-01 12:02:29 +0800

    no message

--- src/common/components/ImHeader.vue
+++ src/common/components/ImHeader.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <x-header :right-options="{showMore: true}" @on-click-more="showMenus = true">所有会话</x-header>
+        <x-header :right-options="{showMore: true}" @on-click-more="showMenus = true">{{ middleText }}</x-header>
     </div>
 </template>
 
@@ -8,13 +8,22 @@
   import { XHeader } from 'vux'
   export default {
     name: 'ImHeader',
+    props: {
+      middleText: {
+        type: String,
+        default: function () {
+          return '所有会话'
+        }
+      }
+    },
     data () {
       return {
         menus: {
           menu1: 'Take Photo',
           menu2: 'Choose from photos'
         },
-        showMenus: false
+        showMenus: false,
+        middleText: this.middleText
       }
     },
     components: {
--- src/views/home/Chat.vue
+++ src/views/home/Chat.vue
@@ -1,20 +1,64 @@
 <template>
     <div>
-        <im-header></im-header>
-
+        <im-header v-bind:middleText="headerText"></im-header>
+        <div id="chat-container">
+            <div class="chat-content">
+                <div class="chat-msg">
+                    <flexbox :gutter="0">
+                        <flexbox-item :span="2">
+                            <div class="media-avatar">
+                                <a href="#">
+                                    <img v-bind:src="avatar" alt="">
+                                </a>
+                            </div>
+                        </flexbox-item>
+                        <flexbox-item :span="8">
+                            <div class="media-body">
+                                <ul>
+                                    <li>
+                                        <p>Amy</p>
+                                    </li>
+                                    <li>
+                                        在莲花山宫公园。。。。
+                                    </li>
+                                </ul>
+                            </div>
+                        </flexbox-item>
+                    </flexbox>
+                </div>
+            </div>
+            <div class="chat-send"></div>
+        </div>
     </div>
 </template>
 
 <script>
   import ImHeader from '@/common/components/ImHeader'
+  import {Flexbox, FlexboxItem} from 'vux'
   export default {
     name: 'Chat',
+    data () {
+      return {
+        headerText: '嘻嘻和他的朋友们',
+        avatar: require('@/assets/avater.png')
+      }
+    },
     components: {
-      ImHeader
+      ImHeader,
+      Flexbox,
+      FlexboxItem
     }
   }
 </script>
 
-<style scoped>
-
+<style scoped  lang="less">
+    #chat-container {
+        .chat-content {
+            width: 100%;
+            height: 90%;
+            .chat-msg {
+                padding: 5px 5px;
+            }
+        }
+    }
 </style>
